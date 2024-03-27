c22753454737d5e765404c3c1e0185d30047191d
Richard <richard@communiteq.com>
2024-03-27 19:24:08 +0000

    Clean up

--- assets/javascripts/discourse/connectors/category-custom-security/private-replies.js
+++ assets/javascripts/discourse/connectors/category-custom-security/private-replies.js
@@ -2,7 +2,6 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from "@ember/object";
 import { inject as service } from "@ember/service"
-import Group from "discourse/models/group";
 
 export default class PrivateReplies extends Component {
     @service siteSettings;
